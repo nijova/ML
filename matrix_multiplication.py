079459ef4b4c64c82f39afdff0b65b2f4025afc8
Niko Jovan <niko.jovan@protonmail.com>
2021-05-28 22:18:18 +0000

    implement matrix multiplication

---
+++ matrix_multiplication.py
@@ -0,0 +1,16 @@
+def matmul(m,n):
+  result = []
+  for r in range(m.shape[0]):
+    for c in range(n.shape[1]):
+      sum = 0
+      for x in range(len(m[r])):
+        sum += m[r,x] * n[x,c]
+      result.append(sum)
+  return np.array(result).reshape(m.shape[0], n.shape[1])
+
+a=np.arange(16).reshape(8,2)
+b=np.arange(8).reshape(2,4)
+
+matmul(a, b)
+
+np.matmul(a, b)
